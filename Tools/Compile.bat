bfeeeb4667c468309459921e68d216f82da1ca86
Alex E.T. Snyder <alexetsnyder@gmail.com>
2026-04-12 16:13:42 -0400

    Created CMake Function to Compile Shaders

---
+++ Tools/Compile.bat
@@ -0,0 +1,1 @@
+%VULKAN_SDK%/Bin/slangc.exe ../Assets/Shaders/shader.slang -target spirv -profile spirv_1_4 -emit-spirv-directly -fvk-use-entrypoint-name -entry vertMain -entry fragMain -o slang.spv 
